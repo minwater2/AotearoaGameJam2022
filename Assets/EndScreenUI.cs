7d79b0afd009d6ccadb6e669ae1d3ac608188c0c
Benjamin McNeill <frozenwispstudios@gmail.com>
2022-05-21 14:54:58 +1200

    UI for win/losing yay

---
+++ Assets/EndScreenUI.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Team {Shepherd,Wolf};
+public class EndScreenUI : MonoBehaviour
+{
+    private Team winningteam;
+    [SerializeField] private GameObject _endGameUI;
+    [SerializeField] private GameObject _shepherdWin;
+    [SerializeField] private GameObject _wolfWin;
+    
+    public void WinScreen(Team winner)
+    {
+        _endGameUI.SetActive(true);
+        if (winner == Team.Shepherd)_shepherdWin.SetActive(true);
+        else _wolfWin.SetActive(true);
+        
+        //Show stats here
+    }
+}
