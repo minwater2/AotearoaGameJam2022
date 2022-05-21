a4915e6b5a975c615759bfde8e1fbc2569c17c63
Mike Lee <mike.lee@untamedisles.com>
2022-05-21 14:50:47 +1200

    Shotgun and player movement update

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+using Photon.Pun;
+using UnityEngine;
+
+
+[RequireComponent(typeof(ActionController))]
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviourPun
+{
+    [SerializeField] private float _moveSpeed;
+
+    private ActionController _actionController;
+    private PlayerController _playerController;
+    
+    private Camera _camera;
+    private Vector3 _mousePos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main;
+        _playerController = GetComponent<PlayerController>();
+        _actionController = GetComponent<ActionController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            ProcessInput();
+            
+            Rotate();
+
+            PerformAction();
+        }
+    }
+
+    private void PerformAction() => _actionController.PerformAction();
+
+    private void ProcessInput()
+    {
+        var movement = new Vector3
+        {
+            x = Input.GetAxisRaw("Horizontal"),
+            y = 0f,
+            z = Input.GetAxisRaw("Vertical"),
+        }.normalized;
+
+        Vector3 moveVelocity = movement.normalized * _moveSpeed;
+        _playerController.Move(moveVelocity);
+    }
+    
+    private void Rotate()
+    {
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        
+        if (groundPlane.Raycast(ray, out float rayDistance))
+        {
+            var point = ray.GetPoint(rayDistance);
+            _playerController.LookAt(point);
+        }
+    }
+
+
+}
