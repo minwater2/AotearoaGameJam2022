b112590fe62863a42e61d4c9d0fee064e311346f
Kees Untamed <keest@protonmail.com>
2022-05-22 17:46:49 +1200

    better clicks

--- Assets/ButtonClickSound.cs
+++ Assets/ButtonClickSound.cs
@@ -5,19 +5,20 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Button))]
 public class ButtonClickSound : MonoBehaviour
 {
-    private AudioSource _audioSource;
+    [SerializeField] private AudioSource _audioSourceCustom;
+    private AudioSource _source;
     private Button _button;
     [SerializeField] private AudioClip _buttonClick;
     
     private void Awake()
     {
-        _audioSource = GetComponent<AudioSource>();
+        _source = _audioSourceCustom ? _audioSourceCustom : GetComponent<AudioSource>();
         _button = GetComponent<Button>();
         _button.onClick.AddListener(PlayButtonClick);
     }
 
     private void PlayButtonClick()
     {
-        _audioSource.PlayOneShot(_buttonClick, 0.25f);
+        _source.PlayOneShot(_buttonClick, 0.25f);
     }
 }
